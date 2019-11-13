f66b558c9f5f4e34220efc2f1d5adbbdabb737c3
RusLab <ruslab@imacalamezlocal.local>
2019-11-13 11:40:00 +0300

    My project setup

---
+++ SevostianovaEP_HW2.1/SevostianovaEP_HW2.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SevostianovaEP_HW2.1
+//
+//  Created by RusLab on 13.11.2019.
+//  Copyright © 2019 RusLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
